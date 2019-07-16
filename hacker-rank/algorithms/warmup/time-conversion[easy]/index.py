68361b8289ffd2e8bf668b33cfb9d35a8102a7e1
Sandon <the_sandon@hotmail.com>
2019-07-16 20:58:33 +0800

    finish

--- hacker-rank/algorithms/warmup/time-conversion[easy]/index.py
+++ hacker-rank/algorithms/warmup/time-conversion[easy]/index.py
@@ -12,12 +12,10 @@ def timeConversion(s):
     second = parts[2][0:2]
     tag = parts[2][2:4]
     # print(hour, minute, second, tag)
-    if (tag == 'AM'):
-        if (hour == '12'):
-            hour = '00'
-    if (tag == 'PM'):
-        if (hour != '12'):
-            hour = int(hour) + 12
+    if tag == 'AM' and hour == '12':
+        hour = '00'
+    if tag == 'PM' and hour != '12':
+        hour = int(hour) + 12
 
     return str(hour) + ':' + minute + ':' + second
 
