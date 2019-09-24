0fe3222f3a12c2d0f4e86733a216851a02ad51e0
Sandon <the_sandon@hotmail.com>
2019-09-24 19:25:01 +0800

    update

--- leetcode/median-of-two-sorted-arrays/index.js
+++ leetcode/median-of-two-sorted-arrays/index.js
@@ -1,7 +1,6 @@
 /**
- * Created by Sandon on 2016/4/15.
+ * Created by Sandon on 2019-09-18.
  */
-
 // in-place version of finding the n-th element
 function findNthInPlace(arr1, arr2, arr1StartIndex, arr2StartIndex, arr1Len, arr2Len, n) {
   if (arr1Len + arr2Len < n)
@@ -12,7 +11,7 @@ function findNthInPlace(arr1, arr2, arr1StartIndex, arr2StartIndex, arr1Len, arr
     return arr1[arr1StartIndex + n - 1]
   if (n === 1)
     return  arr1[arr1StartIndex] <= arr2[arr2StartIndex] ? arr1[arr1StartIndex] : arr2[arr2StartIndex]
-  
+
   let arr1LeftLen
   let arr2LeftLen
   if (arr1Len < arr2Len) {
@@ -22,7 +21,8 @@ function findNthInPlace(arr1, arr2, arr1StartIndex, arr2StartIndex, arr1Len, arr
     arr2LeftLen = arr2Len < n ? arr2Len : Math.floor(n/2)
     arr1LeftLen = n - arr2LeftLen
   }
-  
+
+
   const lastOfArr1Left = arr1[arr1StartIndex + arr1LeftLen - 1]
   const lastOfArr2Left = arr2[arr2StartIndex + arr2LeftLen - 1]
   if (lastOfArr1Left === lastOfArr2Left) {
@@ -46,10 +46,37 @@ function findNthInPlace(arr1, arr2, arr1StartIndex, arr2StartIndex, arr1Len, arr
   }
 }
 
+var findMedianSortedArrays = function(nums1, nums2) {
+  const len = nums1.length + nums2.length
+  if (len % 2 === 0) {
+    const index = len / 2
+    const num1 = findNthInPlace(nums1, nums2, 0, 0, nums1.length, nums2.length, index)
+    const num2 = findNthInPlace(nums1, nums2, 0, 0, nums1.length, nums2.length, index + 1)
+    // console.log(index, num1, num2)
+    return (num1 + num2) / 2
+  } else {
+    const index = Math.floor(len / 2)
+    return findNthInPlace(nums1, nums2, 0, 0, nums1.length, nums2.length, index + 1)
+  }
+}
+
 /*
  * test
  */
-const arr1 = [1, 3, 6, 9]
-const arr2 = [2, 5, 8, 10, 12]
-const n = 4
-console.log(findNthInPlace(arr1, arr2, 0, 0, arr1.length, arr2.length, n) === 5)
+// const arr1 = [1, 3, 6, 9]
+// const arr2 = [2, 5, 8, 10, 12]
+// const n = 4
+// console.log(findNthInPlace(arr1, arr2, 0, 0, arr1.length, arr2.length, n) === 5)
+
+// let nums1 = [1, 3]
+// let nums2 = [2]
+// console.log(findMedianSortedArrays(nums1, nums2))
+//
+// nums1 = [1, 2]
+// nums2 = [3, 4]
+// console.log(findMedianSortedArrays(nums1, nums2))
+
+let nums1 = [2,3,4]
+let nums2 = [1]
+console.log(findMedianSortedArrays(nums1, nums2))
+console.log(findNthInPlace(nums1, nums2, 0, 0, nums1.length, nums2.length, 3))
